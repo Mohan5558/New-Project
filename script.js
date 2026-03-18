cbf75d7c567b36b769ec850300eb65497e4347d8
Mohankrishna <mohankrishnakudupudi@gmail.com>
2026-03-18 20:43:16 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToLogin() {
+    window.location.href = "login.html";
+}
+
+function goToSignup() {
+    window.location.href = "signup.html";
+}
+
+function goToProfile() {
+    window.location.href = "profile.html";
+}
